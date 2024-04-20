6cbb556466f7c11b000374346eca8649d701e094
jiyan <144628293+serialcurse@users.noreply.github.com>
2024-04-21 01:43:21 +0300

    the comment lines of the code translated to english

--- rockpaperscissor.py
+++ rockpaperscissor.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-""" Oyuncuların yapabilecekleri seçimler tanımlanıyor."""
+""" Defining the choices players can make."""
 player_1_actions = ["rock", "paper", "scissors"]
 player_2_actions = ["rock", "paper", "scissors"]
 
@@ -9,13 +9,12 @@ rounds = int(input("How many rounds you wanna play? : "))
 score_of_the_player_1 = 0
 score_of_the_player_2 = 0
 
-"""For Döngüsü """
+"""For Loop"""
 for round_num in range(1, rounds + 1):
     print("Round:", round_num)
     time.sleep(2)
     """
-    Oyuncuların yapabilecekleri seçimler arasından rastgele olarak biri seçiliyor ve oyuncuların 
-    yaptığı rastgele seçime değişken tanımlanıyor.
+    One of the choices that players can make is randomly selected and a variable is defined for the random choice made by the players.
     """
     player_1_choice = random.choice(player_1_actions)
     player_2_choice = random.choice(player_2_actions)
@@ -23,12 +22,12 @@ for round_num in range(1, rounds + 1):
     print("Player 1 chose:", player_1_choice)
     print("Player 2 chose:", player_2_choice)
 
-    """ - Tie Beraberlik Koşulu ve Bu Durumda Ekrana Verilecek Output """
+    """ Tie Condition and Output to be Displayed in this Condition """
     tie = (player_1_choice == player_2_choice)
     if tie:
         print("Tie! Both players chose the same action.")
 
-    """Oyuncuların kazanma durumları ve bunları sağlayan koşullar"""
+    """Winning situations of players and the conditions that enable them"""
     if (player_1_choice == "paper" and player_2_choice == "rock") or \
             (player_1_choice == "scissors" and player_2_choice == "paper") or \
             (player_1_choice == "rock" and player_2_choice == "scissors"):
